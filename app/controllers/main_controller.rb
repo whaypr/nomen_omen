2124bf58dfd6ddb273c3892649b08a1a5a506263
Patrik Drbal <drbalpat@fit.cvut.cz>
2022-07-07 15:03:28 +0200

    Update page layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,30 +3,33 @@ require 'json'
 
 class MainController < ApplicationController
   def index  
+    @name = ""
+    @country_code = ""
     @gender = Hash.new
     @age = Hash.new
     @nationality = Hash.new
 
     return if params[:name] == nil or params[:name] == ""
-    params[:country_code] = "US" if params[:country_code] == ""
+    params[:country_code] = "CZ" if params[:country_code] == ""
 
-    params[:country_code] = params[:country_code].upcase
+    @name = params[:name]
+    @country_code = params[:country_code]
 
     # get gender
     response = Net::HTTP.get_response(
-      URI("https://api.genderize.io?name=#{params[:name]}&country_id=#{params[:country_code]}")
+      URI("https://api.genderize.io?name=#{@name}&country_id=#{@country_code}")
     )
     @gender = JSON.parse(response.body)
 
     # get age
     response = Net::HTTP.get_response(
-      URI("https://api.agify.io?name=#{params[:name]}&country_id=#{params[:country_code]}")
+      URI("https://api.agify.io?name=#{@name}&country_id=#{@country_code}")
     )
     @age = JSON.parse(response.body)
 
     # get nationality
     response = Net::HTTP.get_response(
-      URI("https://api.nationalize.io?name=#{params[:name]}&country_id=#{params[:country_code]}")
+      URI("https://api.nationalize.io?name=#{@name}&country_id=#{@country_code}")
     )
     @nationality = JSON.parse(response.body)
   end
